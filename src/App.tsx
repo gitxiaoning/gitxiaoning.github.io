b83c05fc7c6ba6f8321e912d3d658e3122e4ee65
Xiaoning Wang <xiaoningwang010314@gmail.com>
2025-03-29 01:35:38 -0500

    grammar error fix

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
           
           <div className="main-view__content-main">
             <p className="main-view__content-text">
-              welcome to my personal website :)<br /> <br /> i'am a phd student in information sciences at the university of illinois urbana-champaign
+              welcome to my personal website :)<br /> <br /> i am a phd student in information sciences at the university of illinois urbana-champaign
               <img src={uiuc} onClick= {() => {openInNewWindow("https://ischool.illinois.edu/")}} className="main-view__content-uiuc" alt="uiuc icon"></img>
               advised by professor ted underwood <br /> <br /> previously, i obtained my bachelor's degree from the university of toronto
               <img src={uoft} onClick= {() => {openInNewWindow("https://web.cs.toronto.edu/")}} className="main-view__content-uoft" alt="uoft icon"></img>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: 'gitxiaoning.github.io',
+  base: '/gitxiaoning.github.io',
 
   plugins: [react()]
 });
